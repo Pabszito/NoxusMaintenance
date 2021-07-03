f220ae87012fc1223d6725df2053fa9df2f7e3fa
Pabszito <37744843+Pabszito@users.noreply.github.com>
2021-07-03 01:49:37 -0300

    Cleanup code

--- src/main/java/org/noxus/noxusmaintenance/commands/MainCommand.java
+++ src/main/java/org/noxus/noxusmaintenance/commands/MainCommand.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 public class MainCommand implements CommandExecutor {
 
-    private FileManager fileManager;
-    private MaintenanceHandler handler;
+    private final FileManager fileManager;
+    private final MaintenanceHandler handler;
 
     public MainCommand(FileManager fileManager, MaintenanceHandler handler) {
         this.fileManager = fileManager;
@@ -23,36 +23,27 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (!(sender.hasPermission("noxusmaintenance.commands"))) {
-
             sender.sendMessage(TextColor.color(fileManager.getLang().getString("error.no-permission")));
             return true;
         }
 
 
         if (!(args.length > 0)) {
-
             for (String line : fileManager.getLang().getStringList("lang.help")) {
-
                 sender.sendMessage(TextColor.color(line));
-
             }
             return true;
 
         }
 
         switch (args[0].toLowerCase()) {
-
             case "reload":
-
                 fileManager.getLang().reload();
                 fileManager.getConfig().reload();
                 sender.sendMessage(TextColor.color(fileManager.getLang().getString("lang.reload")));
                 break;
-
             case "on":
-
                 if (!fileManager.getConfig().getBoolean("config.enable")) {
-
                     fileManager.getConfig().set("config.enable", true);
                     fileManager.getConfig().save();
                     sender.sendMessage(TextColor.color(
@@ -65,9 +56,7 @@ public class MainCommand implements CommandExecutor {
                 break;
 
             case "off":
-
                 if (fileManager.getConfig().getBoolean("config.enable")) {
-
                     fileManager.getConfig().set("config.enable", false);
                     fileManager.getConfig().save();
                     sender.sendMessage(TextColor.color(
@@ -78,82 +67,63 @@ public class MainCommand implements CommandExecutor {
                 sender.sendMessage(TextColor.color(
                         fileManager.getLang().getString("error.already-disable")));
                 break;
-
             case "add":
-
                 if (!(args.length > 1)) {
 
                     for (String line : fileManager.getLang().getStringList("lang.help")) {
-
                         sender.sendMessage(TextColor.color(line));
-
                     }
-                    return true;
 
+                    return true;
                 }
 
                 if (handler.addPlayer(args[1])) {
-
                     sender.sendMessage(fileManager.getLang().getString("lang.player-added").
                             replace("%player%", args[1])
                     );
 
                     return true;
-
                 }
 
                 sender.sendMessage(fileManager.getLang().getString("error.already-player"));
                 break;
-
             case "remove":
-
                 if (!(args.length > 1)) {
-
                     for (String line : fileManager.getLang().getStringList("lang.help")) {
-
                         sender.sendMessage(TextColor.color(line));
-
                     }
-                    return true;
 
+                    return true;
                 }
 
                 if (handler.removePlayer(args[1])) {
-
                     sender.sendMessage(TextColor.color(
                             fileManager.getLang().getString("lang.player-remove").
                                     replace("%player%", args[1])
                     ));
 
                     return true;
-
                 }
 
                 sender.sendMessage(TextColor.color(
                         fileManager.getLang().getString("error.no-player")
                 ));
-
                 break;
-
             case "list":
-
                 StringBuilder listPlayers = new StringBuilder();
                 List<String> whitelist = fileManager.getConfig().getStringList("config.whitelist-player");
+
                 for (int i = 0; i < whitelist.size(); i++) {
-
                     listPlayers.append(whitelist.get(i)).append(' ');
-
                 }
 
-                sender.sendMessage(TextColor.color("&aWhitelist players: &e" + listPlayers.toString()));
-
+                sender.sendMessage(TextColor.color("&aWhitelist players: &e" + listPlayers));
                 break;
             default:
                 for (String line : fileManager.getLang().getStringList("lang.help")) {
-
                     sender.sendMessage(TextColor.color(line));
-
                 }
+
                 break;
         }
         return true;
